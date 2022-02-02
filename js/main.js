2a6e89add2441c57634af5fcfbf1c031883fb1a6
Michał <michalmalicki@op.pl>
2022-02-02 10:55:55 +0100

    finished timer app

--- js/main.js
+++ js/main.js
@@ -20,16 +20,33 @@ let usersTime;
 
 
 
-const appUpdate = () => {
-    eventSpan.textContent = eventName.value ;
-    usersTime = new Date(`${eventMonth.value} ${eventDay.value} ${eventYear.value}`)
-    imageSection.style.backgroundImage = `url(${eventImg.value})`;
+const setTime = () => {
+    const currentTime = new Date();
+    const result = usersTime - currentTime;
 
-    console.log(usersTime);
+    const days = Math.floor(result / 1000 / 60 / 60 / 24);
+    const hours = Math.floor(result / 1000 / 60 / 60 ) % 24;
+    const minutes = Math.floor(result / 1000 / 60) % 60;
+    const seconds = Math.floor(result / 1000 ) % 60;
+    
+    daysCount.textContent = days ;
+    hoursCount.textContent = hours ;
+    minutesCount.textContent = minutes ;
+    secondsCount.textContent = seconds ;
+
 }
 
 
+const appUpdate = () => {
+    eventSpan.textContent = eventName.value ;
+    usersTime = new Date(`${eventMonth.value} ${eventDay.value} ${eventYear.value}`)
+    imageSection.style.backgroundImage = `url('${eventImg.value}')`;
+    setTime();
+}
 
+
+appUpdate();
+setInterval(setTime, 1000);
 
 
 
